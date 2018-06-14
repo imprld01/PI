b3d0351e41cea33459cf7cdc53492f673c12ddf8
Bo-Wun S <bowns1688@hotmail.com>
2018-06-14 22:55:57 +0800

    Update start.py

--- start.py
+++ start.py
@@ -14,21 +14,7 @@ def request_reading():
 reading = request_reading()
 print(reading)
 sys.stdout.flush()
-"""
-while True:
-    command = raw_input("Enter command: ")
-    if command == 'l':
-        bus.write_byte(SLAVE_ADDRESS, ord('l'))
-    if command == 'r':
-        thingspeak_get = 0
-        while True:
-            reading = request_reading()
-            if thingspeak_get == 15:
-                url = "https://api.thingspeak.com/update?api_key=REQ34H0DMYV3WYPV&field1=0"
-                response = requests.request("GET", url)
-                print response.text,
-                print '        '
-                thingspeak_get = 0
-            time.sleep(1)
-            thingspeak_get += 1
-"""
+
+url = "https://api.thingspeak.com/update?api_key=REQ34H0DMYV3WYPV&field1=" + str(reading)
+response = requests.request("GET", url)
+print(response.text)
