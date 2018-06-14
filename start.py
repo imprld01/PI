330b95ee1454abfe248fbd123fa9d4b31d3434d1
Bo-Wun S <bowns1688@hotmail.com>
2018-06-14 23:02:46 +0800

    Update start.py

--- start.py
+++ start.py
@@ -11,9 +11,13 @@ def request_reading():
     reading = int(bus.read_byte(SLAVE_ADDRESS))
     return reading
 
-reading = request_reading()
-print(reading)
-sys.stdout.flush()
+while True:
 
-url = "https://api.thingspeak.com/update?api_key=REQ34H0DMYV3WYPV&field1=" + str(reading)
-response = requests.request("GET", url)
+    reading = request_reading()
+    print(reading)
+    sys.stdout.flush()
+
+    url = "https://api.thingspeak.com/update?api_key=REQ34H0DMYV3WYPV&field1=" + str(reading)
+    response = requests.request("GET", url)
+    
+    reading = -1
