15e72a399f0f79408a685a6a8874693208826dae
Bo-Wun S <bowns1688@hotmail.com>
2018-06-14 23:42:26 +0800

    Update start.py

--- start.py
+++ start.py
@@ -2,22 +2,33 @@ import smbus
 import time
 import requests
 import sys
+import struct
 
 bus = smbus.SMBus(1)
 
 SLAVE_ADDRESS = 0x04
 
+def get_data():
+    return bus.read_i2c_block_data(SLAVE_ADDRESS, 0);
+
+def get_float(data, index):
+    bytes = data[4*index:(index+1)*4]
+    return struct.unpack('f', "".join(map(chr, bytes)))[0]
+
 def request_reading():
-    reading = int(bus.read_byte(SLAVE_ADDRESS))
-    return reading
+    data = get_data()
+    print(get_float(data, 0))
+    print(get_float(data, 1))
+    return 50
+
 
 while True:
 
     reading = request_reading()
-    print(reading)
+    #print(reading)
     sys.stdout.flush()
 
-    url = "https://api.thingspeak.com/update?api_key=REQ34H0DMYV3WYPV&field1=" + str(reading)
-    response = requests.request("GET", url)
+    #url = "https://api.thingspeak.com/update?api_key=REQ34H0DMYV3WYPV&field1=" + str(reading)
+    #response = requests.request("GET", url)
     
     reading = -1
